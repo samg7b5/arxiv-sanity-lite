38442fb5b0e0d848c5445c9f4c887771086ea91e
sam-vps0 <email>
2025-11-26 17:15:13 +0000

    tweak tfidf params to reduce load

--- compute.py
+++ compute.py
@@ -15,7 +15,7 @@ from aslite.db import get_papers_db, save_features
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Arxiv Computor')
-    parser.add_argument('-n', '--num', type=int, default=20000, help='number of tfidf features')
+    parser.add_argument('-n', '--num', type=int, default=5000, help='number of tfidf features')   # default of 20k might be too high for this machine
     parser.add_argument('--min_df', type=int, default=5, help='min df')
     parser.add_argument('--max_df', type=float, default=0.1, help='max df')
     parser.add_argument('--max_docs', type=int, default=-1, help='maximum number of documents to use when training tfidf, or -1 to disable')
--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 docker exec arxiv-sanity python3 arxiv_daemon.py --num 2000
 if [ $? -eq 0 ]; then
     echo "New papers detected! Running compute.py"
-    docker exec arxiv-sanity python3 compute.py
+    docker exec arxiv-sanity python3 compute.py --num 5000 --min_df 6 --max_df 0.15 --max_docs 7000
 else
     echo "No new papers were added"
 fi
